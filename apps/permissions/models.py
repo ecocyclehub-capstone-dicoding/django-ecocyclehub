70cacd34ca63d17ef8ec42327702c0a88b72acf4
Elvandry Ghiffary <elvandry13@gmail.com>
2026-04-22 06:17:47 +0700

    fix: add created_at and updated_at in permission and role models

--- apps/permissions/models.py
+++ apps/permissions/models.py
@@ -6,6 +6,8 @@ class Permission(models.Model):
     name = models.CharField(max_length=100)
     key = models.CharField(max_length=100, unique=True)
     group = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.name)
@@ -16,6 +18,8 @@ class Role(models.Model):
     name = models.CharField(max_length=100)
     key = models.CharField(max_length=50)
     permissions = models.ManyToManyField(Permission)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.name)
