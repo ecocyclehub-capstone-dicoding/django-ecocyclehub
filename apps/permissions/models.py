e4aab678efae23910bbc3fad37a79ad2f99ed737
Elvandry Ghiffary <elvandry13@gmail.com>
2026-04-22 06:01:51 +0700

    feat: add permissions model

--- apps/permissions/models.py
+++ apps/permissions/models.py
@@ -1,3 +1,21 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Permission(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    key = models.CharField(max_length=100, unique=True)
+    group = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Role(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    key = models.CharField(max_length=50)
+    permissions = models.ManyToManyField(Permission)
+
+    def __str__(self):
+        return str(self.name)
