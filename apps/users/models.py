b941222b51a52461ac28e5c9eb0b1e866554da7b
Elvandry Ghiffary <elvandry13@gmail.com>
2026-04-22 06:15:04 +0700

    feat: add user models

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,19 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    username = None  # kita pakai email
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    role = models.ForeignKey('permissions.Role', on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return str(self.email)
