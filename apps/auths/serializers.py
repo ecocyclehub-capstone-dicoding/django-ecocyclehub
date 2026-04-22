c86691c30a00d2449d3d8c932c236b126af24104
Elvandry Ghiffary <elvandry13@gmail.com>
2026-04-22 23:55:18 +0700

    feat: add some code for error response

--- apps/auths/serializers.py
+++ apps/auths/serializers.py
@@ -3,12 +3,39 @@ from apps.users.models import User
 from apps.permissions.models import Role
 
 class RegisterSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+        error_messages={
+            "required": "The name field is required.",
+            "blank": "The name field cannot be empty."
+        }
+    )
+    email = serializers.EmailField(
+        error_messages={
+            "required": "The email field is required.",
+            "blank": "The name field cannot be empty."
+        }
+    )
+    password = serializers.CharField(
+        write_only=True,
+        error_messages={
+            "required": "The password field is required.",
+            "blank": "The name field cannot be empty."
+        }
+    )
+
     class Meta:
         model = User
-        fields = ["id", "name", "email", "password"]
-        extra_kwargs = {
-            "password": {"write_only": True}
-        }
+        fields = ["name", "email", "password"]
+    
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already registered")
+        return value
+    
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Minimum 8 characters")
+        return value
 
     def create(self, validated_data):
         role = Role.objects.get(key="customer")
