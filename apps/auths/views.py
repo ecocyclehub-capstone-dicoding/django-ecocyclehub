4b6b29aaf3510992df2bbceff2073ad584941b63
Elvandry Ghiffary <elvandry13@gmail.com>
2026-04-22 22:23:47 +0700

    fix: override permission for register endpoint

--- apps/auths/views.py
+++ apps/auths/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from .serializers import RegisterSerializer
 
 class RegisterView(APIView):
+    permission_classes = [AllowAny]
+    
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
 
