b3f36bee1d02d4aff7bf57fb1a2afe4468b488f3
Elvandry Ghiffary <elvandry13@gmail.com>
2026-04-22 23:55:52 +0700

    fix: edit error response based on API contract

--- apps/auths/views.py
+++ apps/auths/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
 from apps.users.models import User
+from common.responses import format_error_response
 from .serializers import RegisterSerializer
 from apps.users.serializers import UserResponseSerializer
 
@@ -26,8 +27,35 @@ class RegisterView(APIView):
                 "data": response_serializer.data
             }, status=status.HTTP_201_CREATED)
 
-        return Response({
-            "success": False,
-            "message": "Validation error",
-            "errors": serializer.errors
-        }, status=status.HTTP_400_BAD_REQUEST)
+        # return Response({
+        #     "success": False,
+        #     "message": "Validation error",
+        #     "errors": serializer.errors
+        # }, status=status.HTTP_400_BAD_REQUEST)
+        errors = serializer.errors
+
+        # cek apakah ada error "required"
+        is_required_error = any(
+            "This field is required." in str(err)
+            for field_errors in errors.values()
+            for err in field_errors
+        )
+
+        if is_required_error:
+            return Response(
+                format_error_response(
+                    "Some required fields are missing",
+                    errors,
+                    422
+                ),
+                status=422
+            )
+
+        return Response(
+            format_error_response(
+                "Validation error",
+                errors,
+                400
+            ),
+            status=400
+        )
