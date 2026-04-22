0e1bf0c63022898e43e04f11486112210f672d0f
Elvandry Ghiffary <elvandry13@gmail.com>
2026-04-22 22:55:09 +0700

    fix: edit response data match to API contract

--- apps/auths/views.py
+++ apps/auths/views.py
@@ -2,26 +2,27 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+
+from apps.users.models import User
 from .serializers import RegisterSerializer
+from apps.users.serializers import UserResponseSerializer
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
-    
+
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
 
         if serializer.is_valid():
             user = serializer.save()
+            user = User.objects.select_related("role").prefetch_related("role__permissions").get(id=user.id)
+
+            response_serializer = UserResponseSerializer(user)
 
             return Response({
                 "success": True,
                 "message": "User registered successfully",
-                "data": {
-                    "id": user.id,
-                    "name": user.name,
-                    "email": user.email,
-                    "role": user.role.key
-                }
+                "data": response_serializer.data
             }, status=status.HTTP_201_CREATED)
 
         return Response({
