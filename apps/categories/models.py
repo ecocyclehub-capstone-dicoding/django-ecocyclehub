bd8e4fa5388dfef90a9a1b577e68b849e3c8f1d3
Elvandry Ghiffary <elvandry13@gmail.com>
2026-05-02 17:01:29 +0700

    refactor: Prevent negative persisted values for price and points

--- apps/categories/models.py
+++ apps/categories/models.py
@@ -6,8 +6,8 @@ class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     name = models.CharField(max_length=100)
-    price_per_kg = models.IntegerField()
-    point_per_kg = models.IntegerField()
+    price_per_kg = models.PositiveIntegerField()
+    point_per_kg = models.PositiveIntegerField()
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
