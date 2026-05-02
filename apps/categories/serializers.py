265066263a83d7a2f4d435cfbc5faa4c427e8528
Elvandry Ghiffary <elvandry13@gmail.com>
2026-05-02 10:13:29 +0700

    refactor: edit serializers

--- apps/categories/serializers.py
+++ apps/categories/serializers.py
@@ -2,6 +2,33 @@ from rest_framework import serializers
 from .models import Category
 
 class CategorySerializer(serializers.ModelSerializer):
+
+    name = serializers.CharField(
+        error_messages={
+            "required": "The name field is required.",
+            "blank": "The name field cannot be empty."
+        }
+    )
+
+    price_per_kg = serializers.IntegerField(
+        error_messages={
+            "required": "The price field is required.",
+            "invalid": "Price should a number"
+        }
+    )
+
+    point_per_kg = serializers.IntegerField(
+        error_messages={
+            "required": "The point field is required.",
+            "invalid": "Point should a number"
+        }
+    )
+
     class Meta:
         model = Category
         fields = "__all__"
+
+    def validate_name(self, value):
+        if Category.objects.filter(name__iexact=value).exists():
+            raise serializers.ValidationError("Category already registered")
+        return value
