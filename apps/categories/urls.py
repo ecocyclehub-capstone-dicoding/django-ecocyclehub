ab2a5873b957b407124b3c3733dcc3589396f7cc
Elvandry Ghiffary <elvandry13@gmail.com>
2026-05-02 13:21:51 +0700

    refactor: merge view

--- apps/categories/urls.py
+++ apps/categories/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 from .views import (
-    CategoryCreateView,
-    CategoryListView,
-    CategoryUpdateView,
-    CategoryDeleteView
+    CategoryListCreateView,
+    CategoryUpdateDeleteView,
 )
 
 urlpatterns = [
-    path('', CategoryListView.as_view(), name='category-list'),
-    path('', CategoryCreateView.as_view(), name='category-create'),
-    path('<uuid:pk>/', CategoryUpdateView.as_view(), name='category-update'),
-    path('<uuid:pk>/', CategoryDeleteView.as_view(), name='category-delete'),
+    path('', CategoryListCreateView.as_view(), name='category-list'),
+    path('<uuid:pk>/', CategoryUpdateDeleteView.as_view(), name='category-detail'),
 ]
