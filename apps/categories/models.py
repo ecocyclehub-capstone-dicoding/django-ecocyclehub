61a19c208461a6127a2dd5d94792c313a14fa02e
Elvandry Ghiffary <elvandry13@gmail.com>
2026-05-02 07:26:50 +0700

    feture: add categories models

--- apps/categories/models.py
+++ apps/categories/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    name = models.CharField(max_length=100)
+    price_per_kg = models.IntegerField()
+    point_per_kg = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
