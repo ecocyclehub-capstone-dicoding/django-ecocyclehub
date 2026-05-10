0552b7dbac14a30fd9329af5a78afe7bac1072de
Elvandry Ghiffary <elvandry13@gmail.com>
2026-05-10 18:43:26 +0700

    refactor: Avoid dual sources of truth for permission keys

--- apps/permissions/migrations/0005_seed_permissions.py
+++ apps/permissions/migrations/0005_seed_permissions.py
@@ -1,95 +1,94 @@
 from django.db import migrations
 
+PERMISSIONS = [
+    # Categories
+    {
+        "name": "Add Categories",
+        "key": "add_categories",
+        "group": "categories"
+    },
+    {
+        "name": "View Categories",
+        "key": "view_categories",
+        "group": "categories"
+    },
+    {
+        "name": "Edit Categories",
+        "key": "edit_categories",
+        "group": "categories"
+    },
+    {
+        "name": "Delete Categories",
+        "key": "delete_categories",
+        "group": "categories"
+    },
+
+    # Transactions
+    {
+        "name": "Create Transaction",
+        "key": "create_transaction",
+        "group": "transactions"
+    },
+    {
+        "name": "Verify Transaction",
+        "key": "verify_transaction",
+        "group": "transactions"
+    },
+    {
+        "name": "View Transaction",
+        "key": "view_transaction",
+        "group": "transactions"
+    },
+
+    # Dashboard
+    {
+        "name": "View Customer Dashboard",
+        "key": "view_customer_dashboard",
+        "group": "dashboard"
+    },
+    {
+        "name": "View Admin Dashboard",
+        "key": "view_admin_dashboard",
+        "group": "dashboard"
+    },
+    {
+        "name": "View Officer Dashboard",
+        "key": "view_officer_dashboard",
+        "group": "dashboard"
+    },
+
+    # Gamification
+    {
+        "name": "View Leaderboard",
+        "key": "view_leaderboard",
+        "group": "gamification"
+    },
+    {
+        "name": "Add Level",
+        "key": "add_level",
+        "group": "gamification"
+    },
+    {
+        "name": "View Level",
+        "key": "view_level",
+        "group": "gamification"
+    },
+    {
+        "name": "Edit Level",
+        "key": "edit_level",
+        "group": "gamification"
+    },
+    {
+        "name": "Delete Level",
+        "key": "delete_level",
+        "group": "gamification"
+    },
+]
+
 def seed_permissions(apps, schema_editor):
     Permission = apps.get_model("permissions", "Permission")
 
-    permissions = [
-
-        # Categories
-        {
-            "name": "Add Categories",
-            "key": "add_categories",
-            "group": "categories"
-        },
-        {
-            "name": "View Categories",
-            "key": "view_categories",
-            "group": "categories"
-        },
-        {
-            "name": "Edit Categories",
-            "key": "edit_categories",
-            "group": "categories"
-        },
-        {
-            "name": "Delete Categories",
-            "key": "delete_categories",
-            "group": "categories"
-        },
-
-        # Transactions
-        {
-            "name": "Create Transaction",
-            "key": "create_transaction",
-            "group": "transactions"
-        },
-        {
-            "name": "Verify Transaction",
-            "key": "verify_transaction",
-            "group": "transactions"
-        },
-        {
-            "name": "View Transaction",
-            "key": "view_transaction",
-            "group": "transactions"
-        },
-
-        # Dashboard
-        {
-            "name": "View Customer Dashboard",
-            "key": "view_customer_dashboard",
-            "group": "dashboard"
-        },
-        {
-            "name": "View Admin Dashboard",
-            "key": "view_admin_dashboard",
-            "group": "dashboard"
-        },
-        {
-            "name": "View Officer Dashboard",
-            "key": "view_officer_dashboard",
-            "group": "dashboard"
-        },
-
-        # Gamification
-        {
-            "name": "View Leaderboard",
-            "key": "view_leaderboard",
-            "group": "gamification"
-        },
-        {
-            "name": "Add Level",
-            "key": "add_level",
-            "group": "gamification"
-        },
-        {
-            "name": "View Level",
-            "key": "view_level",
-            "group": "gamification"
-        },
-        {
-            "name": "Edit Level",
-            "key": "edit_level",
-            "group": "gamification"
-        },
-        {
-            "name": "Delete Level",
-            "key": "delete_level",
-            "group": "gamification"
-        },
-    ]
-
-    for permission in permissions:
+    for permission in PERMISSIONS:
         Permission.objects.get_or_create(
             key=permission["key"],
             defaults={
@@ -102,23 +101,7 @@ def seed_permissions(apps, schema_editor):
 def reverse_permissions(apps, schema_editor):
     Permission = apps.get_model("permissions", "Permission")
 
-    keys = [
-        "add_categories",
-        "view_categories",
-        "edit_categories",
-        "delete_categories",
-        "create_transaction",
-        "verify_transaction",
-        "view_transaction",
-        "view_customer_dashboard",
-        "view_admin_dashboard",
-        "view_officer_dashboard",
-        "view_leaderboard",
-        "add_level",
-        "view_level",
-        "edit_level",
-        "delete_level"
-    ]
+    keys = [permission["key"] for permission in PERMISSIONS]
 
     Permission.objects.filter(key__in=keys).delete()
 
