ebd408d1bd3f2795906629a0f4c56eeb0e75eb9a
Elvandry Ghiffary <elvandry13@gmail.com>
2026-05-10 18:28:44 +0700

    feature: add assign permissions to roles

---
+++ apps/permissions/migrations/0006_assign_permissions_to_roles.py
@@ -0,0 +1,92 @@
+from django.db import migrations
+
+def assign_permissions(apps, schema_editor):
+    Role = apps.get_model("permissions", "Role")
+    Permission = apps.get_model("permissions", "Permission")
+
+    customer = Role.objects.get(key="customer")
+    officer = Role.objects.get(key="officer")
+    admin = Role.objects.get(key="admin")
+    superadmin = Role.objects.get(key="superadmin")
+
+    # CUSTOMER
+    customer_permissions = [
+        "create_transaction",
+        "view_transaction",
+        "view_leaderboard",
+        "view_categories",
+        "view_customer_dashboard",
+    ]
+
+    # OFFICER
+    officer_permissions = [
+        "view_categories",
+        "create_transaction",
+        "view_transaction",
+        "verify_transaction",
+        "view_officer_dashboard",
+        "view_leaderboard",
+    ]
+
+    # ADMIN
+    admin_permissions = [
+        "add_categories",
+        "view_categories",
+        "edit_categories",
+        "delete_categories",
+
+        "create_transaction",
+        "view_transaction",
+        "verify_transaction",
+
+        "view_admin_dashboard",
+
+        "add_level",
+        "view_level",
+        "edit_level",
+        "delete_level",
+
+        "view_leaderboard",
+    ]
+
+    # SUPERADMIN
+    superadmin_permissions = Permission.objects.all()
+
+    customer.permissions.set(
+        Permission.objects.filter(
+            key__in=customer_permissions
+        )
+    )
+
+    officer.permissions.set(
+        Permission.objects.filter(
+            key__in=officer_permissions
+        )
+    )
+
+    admin.permissions.set(
+        Permission.objects.filter(
+            key__in=admin_permissions
+        )
+    )
+
+    superadmin.permissions.set(superadmin_permissions)
+
+def reverse_assign_permissions(apps, schema_editor):
+    Role = apps.get_model("permissions", "Role")
+
+    for role in Role.objects.all():
+        role.permissions.clear()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('permissions', '0005_seed_permissions'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            assign_permissions,
+            reverse_assign_permissions
+        ),
+    ]
