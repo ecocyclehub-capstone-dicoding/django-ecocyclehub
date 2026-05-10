9618ca202f56815b4f2fffb2e923722133f5c09d
Elvandry Ghiffary <elvandry13@gmail.com>
2026-05-10 09:23:50 +0700

    feature: add gamification migration

--- apps/gamification/models.py
+++ apps/gamification/models.py
@@ -1,3 +1,22 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Level(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
+    name = models.CharField(max_length=100)
+    min_points = models.IntegerField(unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["min_points"]
+
+    def __str__(self):
+        return self.name
