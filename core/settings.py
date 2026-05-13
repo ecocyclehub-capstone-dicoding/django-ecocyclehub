4b2b63e2ba74a1b63fe41a4e93f7d9e301a16615
Elvandry Ghiffary <elvandry13@gmail.com>
2026-05-13 21:38:10 +0700

    feature: add token blacklist app

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'drf_spectacular',
+    'rest_framework_simplejwt.token_blacklist',
     # apps
     'apps.users',
     'apps.auths',
