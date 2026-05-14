d80a4dd0a48effbc5b02d9c12febe87559e9a27b
Elvandry Ghiffary <elvandry13@gmail.com>
2026-05-14 20:19:39 +0700

    refactor: refactor code

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
+from django.db import IntegrityError, transaction
 from apps.permissions.models import Role
 from apps.permissions.serializers import RoleSerializer
 from .models import User
@@ -11,12 +14,39 @@ class UserResponseSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "email", "role"]
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=False)
+    name = serializers.CharField(
+        error_messages={
+            "required": "The name field is required.",
+            "blank": "The name field cannot be empty."
+        }
+    )
+
+    email = serializers.EmailField(
+        error_messages={
+            "required": "The email field is required.",
+            "blank": "The email field cannot be empty."
+        }
+    )
+
+    password = serializers.CharField(
+        write_only=True,
+        required=False,
+        error_messages={
+            "required": "The password field is required.",
+            "blank": "The password field cannot be empty."
+        }
+    )
+
     role_id = serializers.PrimaryKeyRelatedField(
-        queryset=Role.objects.all(),
+        queryset=Role.objects.filter(
+            key__in=["customer", "officer", "admin"]
+        ),
         source="role",
         write_only=True,
-        required=False
+        required=True,
+        error_messages={
+            "required": "The role_id field is required."
+        }
     )
 
     class Meta:
@@ -30,25 +60,64 @@ class UserSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ["id"]
 
+    def validate_email(self, value):
+        queryset = User.objects.filter(email=value)
+
+        if self.instance:
+            queryset = queryset.exclude(id=self.instance.id)
+
+        if queryset.exists():
+            raise serializers.ValidationError(
+                "Email already registered"
+            )
+
+        return value
+
+    def validate_password(self, value):
+        try:
+            validate_password(value)
+        except DjangoValidationError as exc:
+            raise serializers.ValidationError(
+                list(exc.messages)
+            ) from exc
+
+        return value
+
     def create(self, validated_data):
-        password = validated_data.pop("password", None)
+        try:
+            with transaction.atomic():
+                return User.objects.create_user(
+                    email=validated_data["email"],
+                    password=validated_data["password"],
+                    name=validated_data["name"],
+                    role=validated_data["role"],
+                )
 
-        user = User(**validated_data)
-
-        if password:
-            user.set_password(password)
-
-        user.save()
-
-        return user
+        except IntegrityError as exc:
+            raise serializers.ValidationError(
+                {"email": "Email already registered"}
+            ) from exc
 
     def update(self, instance, validated_data):
         password = validated_data.pop("password", None)
 
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
+        instance.name = validated_data.get(
+            "name",
+            instance.name
+        )
+
+        instance.email = validated_data.get(
+            "email",
+            instance.email
+        )
+
+        instance.role = validated_data.get(
+            "role",
+            instance.role
+        )
 
         if password:
+            validate_password(password)
             instance.set_password(password)
 
         instance.save()
