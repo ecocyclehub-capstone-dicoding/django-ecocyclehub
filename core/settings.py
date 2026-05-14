a692a2c1b41318e80f6a8abbd53b1ba96dc2416f
Elvandry Ghiffary <elvandry13@gmail.com>
2026-05-14 10:24:33 +0700

    hotfix: change allowed host

--- core/settings.py
+++ core/settings.py
@@ -25,12 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "ecocyclehub-capstone-dicoding-ecocyclehub-api.hf.space",
+]
 
 
 # Application definition
