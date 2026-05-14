d8dd4264708df5802c4fee5775a4cc44a8b867cd
Elvandry Ghiffary <elvandry13@gmail.com>
2026-05-14 19:48:54 +0700

    feature: add user serializer

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from apps.permissions.models import Role
 from apps.permissions.serializers import RoleSerializer
 from .models import User
 
@@ -8,3 +9,48 @@ class UserResponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "name", "email", "role"]
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
+    role_id = serializers.PrimaryKeyRelatedField(
+        queryset=Role.objects.all(),
+        source="role",
+        write_only=True,
+        required=False
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "name",
+            "email",
+            "password",
+            "role_id"
+        ]
+        read_only_fields = ["id"]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+
+        user = User(**validated_data)
+
+        if password:
+            user.set_password(password)
+
+        user.save()
+
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+
+        return instance
