433a283ebc96d4b9c6044602954f3316cc0c3265
Elvandry Ghiffary <elvandry13@gmail.com>
2026-05-16 21:52:23 +0700

    feature: add management view urls

--- apps/transactions/urls.py
+++ apps/transactions/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TransactionListCreateView, TransactionVerifyView
+from .views import TransactionListCreateView, TransactionVerifyView, TransactionManagementView
 
 urlpatterns = [
     path('', TransactionListCreateView.as_view(), name='transaction-list'),
     path('<uuid:pk>/verify/', TransactionVerifyView.as_view(), name='transaction-verify'),
+    path('all/',TransactionManagementView.as_view(),name='transaction-management-list'),
+    path('all/<uuid:pk>/',TransactionManagementView.as_view(),name='transaction-management-detail'),
 ]
